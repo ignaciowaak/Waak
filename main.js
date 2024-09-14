c7d4d8291d8ad7547c45185afc7e68d9e6e07f3a
Ignaciowaak <binancecrypto9733@gmail.com>
2024-09-13 22:17:07 -0300

    update main.js

--- main.js
+++ main.js
@@ -28,10 +28,10 @@ const songs = [
   { title: 'Nightcrawler travis scott', src: 'cansiones/6.mp3', img: 'fotos/6.jpg' },
   { title: 'Travis Scott - TIL FURTHER NOTICE ft. James Blake_ 21 Savage', src: 'cansiones/7.mp3', img: 'fotos/7.jpg' },
   { title: 'Travis Scott - HIGHEST IN THE ROOM.mp3', src: 'cansiones/8.mp3', img: 'fotos/8.jpg' },
+    { title: 'Canción 9', src: 'cansiones/8.mp3', img: 'fotos/8.jpg' },
     { title: 'Canción 10', src: 'cansiones/8.mp3', img: 'fotos/8.jpg' },
     { title: 'Canción 11', src: 'cansiones/8.mp3', img: 'fotos/8.jpg' },
-    { title: 'Canción 12', src: 'cansiones/8.mp3', img: 'fotos/8.jpg' },
-    { title: 'Canción 13', src: 'cansiones/8.mp3', img: 'fotos/13.jpg' }
+    { title: 'Canción 12', src: 'cansiones/8.mp3', img: 'fotos/13.jpg' }
 ];
 
 let currentSongIndex = 0;
